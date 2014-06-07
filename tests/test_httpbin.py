3de8e6e2dd0ea0b126ee09eb576858cfc120c5e4
Kevin McCarthy <me@kevinmccarthy.org>
2014-06-07 12:55:48 -1000

    test lots of requests

--- tests/test_httpbin.py
+++ tests/test_httpbin.py
@@ -8,3 +8,7 @@ def test_httpbin_accepts_get_requests(httpbin):
 
 def test_httpbin_secure_accepts_get_requests(httpbin_secure):
     assert requests.get(httpbin_secure.url + '/get').status_code == 200
+
+def test_httpbin_secure_accepts_lots_of_get_requests(httpbin_secure):
+    for i in range(10):
+        assert requests.get(httpbin_secure.url + '/get').status_code == 200
