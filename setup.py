4beb2c05bfdf2e703de03611fbeef140ad37ab8f
Marc Abramowitz <marc@marc-abramowitz.com>
2014-11-30 21:29:43 -0800

    setup.py: Get version from version.py
    This creates a new file, pytest_httpbin/version.py and makes setup.py
    execute that file rather than doing an `import pytest_httpbin` which
    causes the setup.py to depend on pytest being installed.
    
    Fixes: #9

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ import codecs
 import os
 import re
 
-import pytest_httpbin
+with open("pytest_httpbin/version.py") as f:
+    code = compile(f.read(), "pytest_httpbin/version.py", 'exec')
+    exec(code)
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -16,7 +18,7 @@ setup(
 
     # There are various approaches to referencing the version. For a discussion,
     # see http://packaging.python.org/en/latest/tutorial.html#version
-    version=pytest_httpbin.__version__,
+    version=__version__,
 
     description="Easily test your HTTP library against a local copy of httpbin",
     long_description=long_description,
